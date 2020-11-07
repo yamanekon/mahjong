d02170af547d741aa7fffae037da7e6419342086
yamane <bravoski.recco@gmail.com>
2020-11-07 16:29:10 +0900

    first commit

---
+++ src/main/java/com/example/mahjong/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.mahjong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+
+		return "index";
+
+	}
+}
---
+++ src/test/java/com/example/mahjong/MahjongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mahjong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
